06f6c07e09f41a6d8570fbac876a408c137c9423
Abdullah Aslam <aslamabdullah724@gmail.com>
2024-03-28 19:58:14 +0500

    whatsapp done

--- src/Whatsapp.js
+++ src/Whatsapp.js
@@ -6,7 +6,7 @@ const WhatsAppButton = ({ }) => {
 
   return (
     <div id="whatsapp-button" class="whatsapp-button ">
-      <a href="https://wa.me/+441332200205?text=Hello%20there!" target="_blank">
+      <a href="https://wa.me/+447445313937?text=Hello%20there!" target="_blank">
         <img src="https://ik.imagekit.io/mctozv7td/Vaping%20Circle/whatsapp.png?updatedAt=1711462415230" alt="WhatsApp Icon" />
       </a>
     </div>
