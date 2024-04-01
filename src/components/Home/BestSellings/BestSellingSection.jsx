2fe7996ebc2af1ebad453db724043d0f080b4f42
Abdullah Aslam <aslamabdullah724@gmail.com>
2024-04-01 05:01:43 +0500

    new

--- src/components/Home/BestSellings/BestSellingSection.jsx
+++ src/components/Home/BestSellings/BestSellingSection.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import SellingCard from "./SellingCard";
 import { bestSelling } from "../../../data/BestSelling";
+import { Link } from "react-router-dom";
+import MainBtn from "../../shared/MainBtn";
 
 const BestSellingSection = () => {
   return (
     <>
-      <div className=" flex flex-col text-left  gap-8 md:px-[60px] px-[16px] my-20">
+      <div className=" flex flex-col text-left  mt-[5rem] gap-8 md:px-[60px] px-[16px] my-10">
         {/* HEADINGS SECTION */}
         <div className=" flex flex-col gap-1 text-left">
 
-          <h3 className="md:text-4xl text-2xl text-headingColor text-left">
-            Our Jewllery Design
+          <h3 className="md:text-4xl text-2xl text-headingColor mt-5 text-left">
+            We can create your imagination
+
           </h3>
+          <p>Bring your imagination today into our store, and we will help you bring that to life in the most beautiful form you can think of. We have many years of experience in crafting custom designs for our customers.</p>
         </div>
 
         {/* CARDS SECTION */}
@@ -22,6 +26,15 @@ const BestSellingSection = () => {
               <SellingCard key={i} {...item} />
             ))}
           </div>
+        </div>
+        <div className=" flex bg-white   justify-center items-center">
+          <Link to='./CollectionPage'>
+            <button className=' text-center bg-[#c8c9ca] px-[80px] py-[10px] text-black text-xl hover:text-black hover:bg-grey transition-all duration-300 ease-in-out transform-gpu hover:scale-105 group'>
+              <span className='transform translate-y-[8] transition duration-300 ease-in-out group-hover:translate-y-[200px]'>
+                View All
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </>
