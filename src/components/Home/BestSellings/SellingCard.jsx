7075a49fada0eecf5286f3f3af04b281c20066cf
Abdullah Aslam <aslamabdullah724@gmail.com>
2024-04-02 14:23:26 +0500

    new

--- src/components/Home/BestSellings/SellingCard.jsx
+++ src/components/Home/BestSellings/SellingCard.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SellingCard = ({ img, title }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className="mb-5 flex flex-col  gap-2 text-left md:w-full w-60 xl:px-4 md:px-3 px-3 transition-transform duration-300 ease-in-out transform hover:scale-105">
-      <div className=" sellingCard rounded relative">
-        <img src={img} alt=" img" className="rounded w-[15rem] h-60" />
-        {/* <div className=" sellingCard-hover-div">
-          <p className="w-full text-center text-sm font-semibold uppercase">Add To Cart</p>
-        </div> */}
+    <div className="mb-5 flex flex-col gap-2 text-left md:w-full w-60 xl:px-4 md:px-3 px-3">
+      <div className="sellingCard rounded relative" onClick={toggleModal}>
+        <img src={img} alt="img" className="rounded w-[15rem] h-60" />
       </div>
-      {/* <h2 className=" xl:text-xl text-base font-medium mx-auto text-left">
+      {isModalOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 z-50"
+          onClick={toggleModal}
+        >
+          <img src={img} alt="img" className="rounded-lg md:w-[30rem] w-[16rem]  h-[16rem] md:h-[30rem]" />
+          <button
+            className="absolute top-2 right-2 btn-sm btn bg-white text-gray-700"
+            onClick={toggleModal}
+          >
+            Close
+          </button>
+        </div>
+      )}
+      {/* <h2 className="xl:text-xl text-base font-medium mx-auto text-left">
         {title}
       </h2> */}
-      {/* <span className=" xl:text-base text-sm text-primaryColor">FROM</span> */}
+      {/* <span className="xl:text-base text-sm text-primaryColor">FROM</span> */}
     </div>
   );
 };
