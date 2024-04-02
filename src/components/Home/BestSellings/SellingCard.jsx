195a8cd65c127814852926ae9f8db636d39d46e2
Abdullah Aslam <aslamabdullah724@gmail.com>
2024-04-02 14:26:03 +0500

    new

--- src/components/Home/BestSellings/SellingCard.jsx
+++ src/components/Home/BestSellings/SellingCard.jsx
@@ -17,13 +17,15 @@ const SellingCard = ({ img, title }) => {
           className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 z-50"
           onClick={toggleModal}
         >
-          <img src={img} alt="img" className="rounded-lg md:w-[30rem] w-[16rem]  h-[16rem] md:h-[30rem]" />
-          <button
-            className="absolute top-2 right-2 btn-sm btn bg-white text-gray-700"
-            onClick={toggleModal}
-          >
-            Close
-          </button>
+          <div className="modalContainer relative md:mr-[0rem] mr-[0rem] bg-white rounded-lg">
+            <img src={img} alt="img" className="rounded-lg md:w-[30rem] w-[16rem]  h-[16rem] md:h-[30rem]" />
+            <button
+              className="absolute top-2 right-2 btn-sm btn bg-white text-gray-700"
+              onClick={toggleModal}
+            >
+              Close
+            </button>
+          </div>
         </div>
       )}
       {/* <h2 className="xl:text-xl text-base font-medium mx-auto text-left">
